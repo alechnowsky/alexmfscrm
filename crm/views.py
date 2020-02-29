5e4ba3e1505d77dad6ce4d36c30e19365bfeaffe
Alex Lechnowsky <alechnowsky@unomaha.edu>
2020-02-28 18:42:38 -0600

    Changed list of 'product' to 'products' to match my request.

--- crm/views.py
+++ crm/views.py
@@ -93,7 +93,7 @@ def product_new(request):
            product = form.save(commit=False)
            product.created_date = timezone.now()
            product.save()
-           product = Product.objects.filter(created_date__lte=timezone.now())
+           products = Product.objects.filter(created_date__lte=timezone.now())
            return render(request, 'crm/product_list.html', {'products': products})
     else:
         form = ProductForm()
